85f6ff910c19afa97ad6bdc3c60afbaf0864f0c9
Amandeep Singh <amandeep87557@gmail.com>
2026-04-09 20:06:44 +0530

    Added role-based permissions

--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render
 from .models import Category, Product, Order, OrderItem
 from .serializers import CategorySerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_permissions(self):
+        if self.get_permissions in ["GET"]:
+            return [IsAuthenticated()] # GET Product -> User allowed
+        return [IsAdminUser()] # POST, UPDATE, DELETE Product -> ADMIN
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
