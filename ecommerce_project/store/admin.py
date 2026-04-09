a153cfb01a010fa7c9e258b95d13a1c97c4c6b13
Amandeep Singh <amandeep87557@gmail.com>
2026-04-09 19:08:35 +0530

    Registered every MODEL into ADMIN PANEL

--- ecommerce_project/store/admin.py
+++ ecommerce_project/store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Category, Product, Order, OrderItem
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'category']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'created_at']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'product', 'quantity']
