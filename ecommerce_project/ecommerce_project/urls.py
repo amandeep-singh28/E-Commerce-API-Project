b005353b5009636d402bb6ce5bdf3a068d4b023a
Amandeep Singh <amandeep87557@gmail.com>
2026-04-09 19:38:34 +0530

    Implemented ViewSets for Category, Product, Order and configured API routes using DefaultRouter

--- ecommerce_project/ecommerce_project/urls.py
+++ ecommerce_project/ecommerce_project/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from store.views import CategoryViewSet, ProductViewSet, OrderViewSet, OrderItemViewSet
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
+router.register('orders', OrderViewSet)
+router.register('orderitems', OrderItemViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
