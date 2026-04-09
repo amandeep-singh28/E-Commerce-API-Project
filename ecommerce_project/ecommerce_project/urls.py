b4f631d1f15fee8d7e54457bc5f7b222750dc350
Amandeep Singh <amandeep87557@gmail.com>
2026-04-09 19:50:08 +0530

    Added URLS for api/login/ and api/refresh/

--- ecommerce_project/ecommerce_project/urls.py
+++ ecommerce_project/ecommerce_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from store.views import CategoryViewSet, ProductViewSet, OrderViewSet, OrderItemViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet)
@@ -27,5 +28,7 @@ router.register('orderitems', OrderItemViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/login/', TokenObtainPairView.as_view()),
+    path('api/refresh/', TokenRefreshView.as_view())
 ]
