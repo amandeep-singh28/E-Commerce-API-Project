b6f3ca120ad2a8b89e32221124553ff83cf6a949
Amandeep Singh <amandeep87557@gmail.com>
2026-04-09 19:33:35 +0530

    Added ViewSet for Category, Product, Order and OrderItem

--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Category, Product, Order, OrderItem
+from .serializers import CategorySerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
