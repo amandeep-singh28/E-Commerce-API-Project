d189b1a61964b43772974ae6d190513ed97012a1
Amandeep Singh <amandeep87557@gmail.com>
2026-04-09 18:55:23 +0530

    Created model for PRODUCT

--- ecommerce_project/store/models.py
+++ ecommerce_project/store/models.py
@@ -7,3 +7,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    name = models.CharField(max_length = 200)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
