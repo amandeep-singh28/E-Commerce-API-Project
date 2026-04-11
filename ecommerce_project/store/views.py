a7697ee17d916ab622ca02f91ac0a8b03de4bd86
Amandeep Singh <amandeep87557@gmail.com>
2026-04-11 18:24:38 +0530

    Added filtering in ProductViewSet

--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -3,6 +3,8 @@ from .models import Category, Product, Order, OrderItem
 from .serializers import CategorySerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
 
@@ -14,6 +16,12 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    filterset_fields = ['category', 'price']
+    search_fields = ['name']
+    ordering_fields = ['price']
 
     def get_permissions(self):
         if self.request.method == 'GET':
