81c78f637378507e6a1f737eb41d52e86dd18a73
Amandeep Singh <amandeep87557@gmail.com>
2026-04-11 18:29:20 +0530

    Automatically attaches logged in user

--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -35,6 +35,9 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
